0ba1ffb246bec4d92b43cd3af223b0aebaa2c11b
DefTnT <jimmi2051@gmail.com>
2019-05-23 23:31:02 +0700

    Update -- Continue Research Dynamic Link NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 // pages/index.js
 import React from 'react'
 import { Link } from '../routes'
-
+import { Route, Redirect, Switch } from "react-router-dom";
 const list = [
-  { name: 'post', pattern: '/posts/:id',page:"/post" },
-  { name: 'user', pattern: '/users/:id',page:'/user' },
-  {
-      name: 'about',
-      pattern: '/abouts',
-      page: '/about'
-  }
+  { name: 'post', id: 1, title: 'Luke Skywalker' },
+  { name: 'post', id: 2, title: 'C-3PO' },
+  { name: 'post', id: 3, title: 'A New Hope' },
+  { name: 'post', id: 4, title: 'The Empire Strikes Back' },
+  { name: 'post', id: 5, title: "abc" },
 ]
 const index = () => {
   return (
-    <ul>
-      
-      <li><Link route="post" id="1"><a>Luke Skywalker</a></Link></li>
+    <>
+      <ul>
+        {list && list.map((item, index) =>
+          (
+            <li key={index}>
+              <Link route={item.name} id={item.id}><a>{item.title}</a></Link>        
+            </li>
+          )
+        )}
+        {/* <li><Link route="post" id="1"><a>Luke Skywalker</a></Link></li>
       <li><Link route="post" id="2"><a>C-3PO</a></Link></li>
       <li><Link route="user" id="1"><a>A New Hope</a></Link></li>
       <li><Link route="user" id="2"><a>The Empire Strikes Back</a></Link></li>
@@ -23,8 +28,9 @@ const index = () => {
         <Link route="about" >
           <a>The Empire Strikes Back and Luke Skywalker</a>
         </Link>
-      </li>
-    </ul>
+      </li> */}
+      </ul>
+    </>
   )
 }
 export default index
--- pages/route.js
+++ pages/route.js
@@ -1,61 +1,20 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
-
-const App = React.createClass({render(){return(<div>App</div>)}})
-const About = React.createClass({render(){return(<div>About</div>)}})
-const NoMatch = React.createClass({render(){return(<div>Nomatch</div>)}})
-
-const Users = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>Users</h1>
-        <div className="master">
-          <ul>
-            {/* use Link to route around the app */}
-            {this.state.users.map(user => (
-              <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
-            ))}
-          </ul>
-        </div>
-        <div className="detail">
-          {this.props.children}
-        </div>
-      </div>
-    )
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+const list = [
+  { name: 'post', pattern: '/posts/:id',page:"/post" },
+  { name: 'user', pattern: '/users/:id',page:'/user' },
+  {
+      name: 'about',
+      pattern: '/abouts',
+      page: '/about'
   }
-})
-
-const User = React.createClass({
-  componentDidMount() {
-    this.setState({
-      // route components are rendered with useful information, like URL params
-      user: findUserById(this.props.params.userId)
-    })
-  },
-
-  render() {
-    return (
-      <div>
-        <h2>{this.state.user.name}</h2>
-        {/* etc. */}
-      </div>
-    )
-  }
-})
-
-// Declarative route configuration (could also load this config lazily
-// instead, all you really need is a single root route, you don't need to
-// colocate the entire config).
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="about" component={About}/>
-      <Route path="users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
-      </Route>
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.getElementById('root'))
+]
+export default function RouteDynamic() {
+  return (
+    <Switch>
+      {list.map((item,index)=>(
+        <Route exact path={item.pattern} component={item.name}/>
+      ))}
+    </Switch>
+  )
+}
